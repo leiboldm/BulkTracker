cd1d5717a07fd994a2d80f2185ac5e22ea5b2fc4
Matt L <leiboldm@umich.edu>
2016-05-30 21:44:07 -0400

    Added titlebar for settings activity

--- app/src/main/java/com/mattleibold/bulktracker/SettingsActivity.java
+++ app/src/main/java/com/mattleibold/bulktracker/SettingsActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by Matt on 5/30/2016.
  */
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +20,19 @@ public class SettingsActivity extends Activity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Utilities.handleOptionsItemSelected(this, id);
+        return super.onOptionsItemSelected(item);
     }
 }
